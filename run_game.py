90d8647b1862face5df40320e14d7c888d029469
betoelmanco123 <robert.rom.es@icloud.com>
2025-10-14 22:20:37 -0600

    a lot to explain

--- run_game.py
+++ run_game.py
@@ -59,14 +59,25 @@ while rows < 720:
     rows += 90
 
 
+# Crear un diccionario para almacenar las hitboxes
+piece_hitboxes = {}
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            mouse_pos = event.pos
+            # Comprobar todas las hitboxes guardadas
+            for piece_pos, rect in piece_hitboxes.items():
+                if rect.collidepoint(mouse_pos):
+                    piece = tablero.positions[piece_pos]
+                    print(f"¡Click en {piece.__class__.__name__} en {piece_pos}!")
+                    # Aquí puedes agregar la lógica de selección de pieza
+    
     pantalla.fill((0, 0, 0))
-    # dibujar cosas aquí...
-    # draw the a grid 8X8
+    piece_hitboxes.clear()  # Limpiar hitboxes anteriores
+    
     for column in range(8):
         for row in range(8):
             square_index = column * 8 + row
@@ -77,66 +88,84 @@ while running:
                 color = (100, 100, 100)
             # draw the pieces
             pygame.draw.rect(pantalla, color, squares[square_index])
-            if tablero.positions[(column, row)]:
-                left = column * 90 + 20
-                top = row * 90 + 20
-            # Pawn----
             current_piece = tablero.positions[(column, row)]
-            if isinstance(current_piece, Pawn):
-                cx = column * BOX_SIZE + BOX_SIZE // 2
-                cy = row * BOX_SIZE + BOX_SIZE // 2 + 5
-                rect = image_pawn_white.get_rect(center=(cx, cy))
-                if current_piece.color == "white":
-                    pantalla.blit(SPRITES[(("white", "pawn"))], rect)
-                else:
-                    pantalla.blit(SPRITES[(("black", "pawn"))], rect)
+            if current_piece:
+                # Calcular posición para cada tipo de pieza
+                if isinstance(current_piece, Pawn):
+                    cx = column * BOX_SIZE + BOX_SIZE // 2
+                    cy = row * BOX_SIZE + BOX_SIZE // 2 + 5
+                    rect = image_pawn_white.get_rect(center=(cx, cy))
+                    # Guardar la hitbox
+                    piece_hitboxes[(column, row)] = rect
+                    # ...resto del código de dibujo...
+                    if current_piece.color == "white":
+                        pantalla.blit(SPRITES[(("white", "pawn"))], rect)
+                    else:
+                        pantalla.blit(SPRITES[(("black", "pawn"))], rect)
 
-            # Rook---
-            if isinstance(current_piece, Rook):
-                cx = column * BOX_SIZE + BOX_SIZE // 2
-                cy = row * BOX_SIZE + BOX_SIZE // 2 + 5
-                rect = image_rook_white.get_rect(center=(cx, cy))
-                if current_piece.color == "white":
-                    pantalla.blit(SPRITES[("white", "rook")], rect)
+                # Rook---
+                elif isinstance(current_piece, Rook):
+                    cx = column * BOX_SIZE + BOX_SIZE // 2
+                    cy = row * BOX_SIZE + BOX_SIZE // 2 + 5
+                    rect = image_rook_white.get_rect(center=(cx, cy))
+                    piece_hitboxes[(column, row)] = rect
+                    if current_piece.color == "white":
+                        pantalla.blit(SPRITES[("white", "rook")], rect)
+                    else:
+                        pantalla.blit(SPRITES[("black", "rook")], rect)
+                # Knight---
+                elif isinstance(current_piece, Knigth):
+                    cx = column * BOX_SIZE + BOX_SIZE // 2 + 7
+                    cy = row * BOX_SIZE + BOX_SIZE // 2 + 6
+                    rect = image_rook_white.get_rect(center=(cx, cy))
+                    piece_hitboxes[(column, row)] = rect
+                     # Guardar la hitbox
+                    if current_piece.color == "white":
+                        pantalla.blit(SPRITES[("white", "knight")], rect)
+                    else:
+                        pantalla.blit(SPRITES[("black", "knight")], rect)
+                # Bishop---
+                elif isinstance(current_piece, Bishop):
+                    cx = column * BOX_SIZE + BOX_SIZE // 2 
+                    cy = row * BOX_SIZE + BOX_SIZE // 2 + 6
+                    rect = image_rook_white.get_rect(center=(cx, cy))
+                    piece_hitboxes[(column, row)] = rect    
+                    if current_piece.color == "white":
+                        pantalla.blit(SPRITES[("white", "bishop")], rect)
+                    else:
+                        pantalla.blit(SPRITES[("black", "bishop")], rect)
+                # queen---
+                elif isinstance(current_piece, Queen):
+                    cx = column * BOX_SIZE + BOX_SIZE // 2
+                    cy = row * BOX_SIZE + BOX_SIZE // 2 - 5
+                    rect = image_rook_white.get_rect(center=(cx, cy))
+                    piece_hitboxes[(column, row)] = rect
+                    if current_piece.color == "white":
+                        pantalla.blit(SPRITES[("white", "queen")], rect)
+                    else:
+                        pantalla.blit(SPRITES[("black", "queen")], rect)
+                # King
+                elif isinstance(current_piece, King):
+                    cx = column * BOX_SIZE + BOX_SIZE // 2
+                    cy = row * BOX_SIZE + BOX_SIZE // 2 + 6
+                    rect = image_rook_white.get_rect(center=(cx, cy))
+                    piece_hitboxes[(column, row)] = rect
+                     # Guardar la hitbox
+                    if current_piece.color == "white":
+                        pantalla.blit(SPRITES[("white", "king")], rect)
+                    else:
+                        pantalla.blit(SPRITES[("black", "king")], rect)
                 else:
-                    pantalla.blit(SPRITES[("black", "rook")], rect)
-            # Knight---
-            if isinstance(current_piece, Knigth):
-                cx = column * BOX_SIZE + BOX_SIZE // 2 + 7
-                cy = row * BOX_SIZE + BOX_SIZE // 2 + 6
-                rect = image_rook_white.get_rect(center=(cx, cy))
-                if current_piece.color == "white":
-                    pantalla.blit(SPRITES[("white", "knight")], rect)
-                else:
-                    pantalla.blit(SPRITES[("black", "knight")], rect)
-            # Bishop---
-            if isinstance(current_piece, Bishop):
-                cx = column * BOX_SIZE + BOX_SIZE // 2 
-                cy = row * BOX_SIZE + BOX_SIZE // 2 + 6
-                rect = image_rook_white.get_rect(center=(cx, cy))
-                if current_piece.color == "white":
-                    pantalla.blit(SPRITES[("white", "bishop")], rect)
-                else:
-                    pantalla.blit(SPRITES[("black", "bishop")], rect)
-            # queen---
-            if isinstance(current_piece, Queen):
-                cx = column * BOX_SIZE + BOX_SIZE // 2
-                cy = row * BOX_SIZE + BOX_SIZE // 2 - 5
-                rect = image_rook_white.get_rect(center=(cx, cy))
-                if current_piece.color == "white":
-                    pantalla.blit(SPRITES[("white", "queen")], rect)
-                else:
-                    pantalla.blit(SPRITES[("black", "queen")], rect)
-            # King
-            if isinstance(current_piece, King):
-                cx = column * BOX_SIZE + BOX_SIZE // 2
-                cy = row * BOX_SIZE + BOX_SIZE // 2 + 6
-                rect = image_rook_white.get_rect(center=(cx, cy))
-                if current_piece.color == "white":
-                    pantalla.blit(SPRITES[("white", "king")], rect)
-                else:
-                    pantalla.blit(SPRITES[("black", "king")], rect)
-
+                    print("pieza no reconocida")
+                    cx = column * BOX_SIZE
+                    cy = row * BOX_SIZE + BOX_SIZE // 2
+                    rect = pygame.Rect(cx, cy, BOX_SIZE, BOX_SIZE)
+                    piece_hitboxes[(column, row)] = rect
+                    pygame.draw.rect(pantalla, (255, 0, 255), rect, 2)  # 2 = grosor
+            # Opcional: Dibujar hitboxes para debug
+            for rect in piece_hitboxes.values():
+                pygame.draw.rect(pantalla, (255, 0, 0), rect, 2)
+    
     pygame.display.update()
 
 pygame.quit()
